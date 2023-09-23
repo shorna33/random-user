81c47de0bc45b03058dd7170e09839a3761a1b06
Samia Shorna <samiasharnami225@gmail.com>
2023-09-23 23:48:25 +0600

    basic table with 100 data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./pages/home";
 
 function App() {
-  return <div className="App">
+  return <div className="App container p-4">
     <Home />
   </div>;
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default Home
+  const baseURL = "https://randomuser.me/api/?results=100";
+
+  //retrive data from api
+  useEffect(() => {
+    axios.get(baseURL).then((res) => {
+      console.log(res.data.results);
+      const users = res.data.results;
+      setUsers(users);
+    });
+  }, []);
+
+  return (
+    <div className="row justify-content-center">
+      <div className="col-lg-8">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Full Name</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Email</th>
+              <th scope="col">Country</th>
+            </tr>
+          </thead>
+          {users.length > 0 ? (
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.phone}>
+                  <td>
+                    {user.name.title} {user.name.first} {user.name.last}
+                  </td>
+                  <td>{user.gender}</td>
+                  <td>{user.email}</td>
+                  <td>{user.location.country}</td>
+                </tr>
+              ))}
+            </tbody>
+          ) : (
+            <tbody>
+                <tr>
+                    <td colSpan="4">No users Found</td>
+                </tr>
+              
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
