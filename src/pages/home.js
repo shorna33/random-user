605b938e3cd3d8a8ae6cf977cffc3bd49c96d6f5
Samia Shorna <samiasharnami225@gmail.com>
2023-09-24 01:53:40 +0600

    pagination

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Pagination } from "@mui/material";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 5;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const currentUsers = users.slice(firstIndex, lastIndex);
+  const noOfPage = Math.ceil(users.length / recordsPerPage);
 
   const baseURL = "https://randomuser.me/api/?results=100";
 
@@ -15,40 +23,60 @@ const Home = () => {
     });
   }, []);
 
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <div className="row justify-content-center">
       <div className="col-lg-8">
         <table className="table">
           <thead>
             <tr>
+              <th scope="col">Id</th>
               <th scope="col">Full Name</th>
               <th scope="col">Gender</th>
               <th scope="col">Email</th>
+              <th scope="col">Phone</th>
               <th scope="col">Country</th>
             </tr>
           </thead>
-          {users.length > 0 ? (
+          {currentUsers.length > 0 ? (
             <tbody>
-              {users.map((user) => (
+              {currentUsers.map((user) => (
                 <tr key={user.phone}>
+                  <td>{user.id.value}</td>
                   <td>
                     {user.name.title} {user.name.first} {user.name.last}
                   </td>
                   <td>{user.gender}</td>
                   <td>{user.email}</td>
+                  <td>{user.phone}</td>
                   <td>{user.location.country}</td>
                 </tr>
               ))}
             </tbody>
           ) : (
             <tbody>
-                <tr>
-                    <td colSpan="4">No users Found</td>
-                </tr>
-              
+              <tr>
+                <td colSpan="6">No users Found</td>
+              </tr>
             </tbody>
           )}
         </table>
+
+        <div className="d-flex justify-content-center pb-3">
+          {users.length > recordsPerPage && (
+            <Pagination
+              color="primary"
+              variant="outlined"
+              shape="rounded"
+              count={noOfPage}
+              page={currentPage}
+              onChange={paginate}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
